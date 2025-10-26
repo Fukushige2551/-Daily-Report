afb360aca51afc7820b5d905eb9b3937856f1a69
Fukushige2551 <k.fukushige@cdrive-inc.jp>
2025-10-26 11:33:48 +0000

    add: 日報に勤務時間の追加

--- next/src/app/modal.jsx
+++ next/src/app/modal.jsx
@@ -1,6 +1,6 @@
 'use client'
-
 import React from "react"
+import '../style/app/dailyReport.scss'
 
 export const Modal = ({
     setModal,
@@ -11,6 +11,25 @@ export const Modal = ({
         setModalDailyReport(false);
     }
 
+    // 今日の日付を取得
+    const today = new Date();
+
+    // 曜日配列
+    const weekdays = ['日', '月', '火', '水', '木', '金', '土'];
+
+    // 年・月・日・曜日を取得
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const date = String(today.getDate()).padStart(2, '0');
+    const weekday = weekdays[today.getDay()];
+
+    const hours = String(today.getHours()).padStart(2, '0');
+    const minutes = String(today.getMinutes()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}`;
+
+    // フォーマット整形
+    const formatted = `${year}年${month}月${date}日(${weekday})`;
+
     return (
         <div className="c-modal">
             <div className="c-modal__body">
@@ -18,6 +37,16 @@ export const Modal = ({
                     <button className="c-btn--general" onClick={() => closeModal()}>閉じる</button>
                     <h3>日報作成</h3>
                     <button className="c-btn--general" onClick={() => closeModal()}>次へ</button>
+                </div>
+                <div className="p-dailyReport">
+                    <dl>
+                        <dt>日付</dt>
+                        <dd>{formatted}</dd>
+                    </dl>
+                    <dl>
+                        <dt>勤務時間</dt>
+                        <dd>10:00 ~ {timeString}</dd>
+                    </dl>
                 </div>
             </div>
         </div>
